9843942d070d94ee072a19c5924f0aebb2a54cd4
AnaduCG <godzionwin@gmail.com>
2023-06-06 22:34:31 +0100

    Update code algorithm

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-for i in range(90, 64, -1):
-    print(chr(i + 32) + chr(i), end='')
-
-print()
-
+for i in reversed(range (97, 123)):
+    if i % 2:
+        i = i - 32
+    print("{}".format(chr(i)), end='')
