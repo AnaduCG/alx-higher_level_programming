c84c7661f099c42ac6ce86d91eae978292ae6463
AnaduCG <godzionwin@gmail.com>
2023-06-06 15:28:11 +0100

    Updating algorithmm for proper negative int handling

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,18 +1,15 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-last = str(number)
-
-if number > 0 and last[-1] > '5':
-    print("Last digit of {} is {} and is greater than 5\
-            ".format(number, last[-1]))
-elif last[-1] == '0':
-    print("Last digit of {} is {} and is 0\
-            ".format(number, last[-1]))
-elif last[-1] < '6' and last[-1] != '0':
+last = int(str(number)[-1])
+if number < 0:
+    last -= (last * 2)
+if last > 5:
+    print("Last digit of {} is {} and is greater than 5".format(number, last))
+elif last == 0:
+    print("Last digit of {} is {} and is 0".format(number, last))
+elif last < 6 and last != 0:
     if number < 0:
-        print("Last digit of {} is -{} and is less than 6 and not 0\
-                ".format(number, last[-1]))
+        print("Last digit of {} is {} and is less than 6 and not 0".format(number, last))
     else:
-        print("Last digit of {} is {} and is less than 6 and....not 0\
-                ".format(number, last[-1]))
+        print("Last digit of {} is {} and is less than 6 and not 0".format(number, last))
