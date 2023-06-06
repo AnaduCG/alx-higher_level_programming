63a88dc210d8486dac6f54bda6b119b091b35b36
AnaduCG <godzionwin@gmail.com>
2023-06-06 18:31:52 +0100

    Pushing to github

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    for i in range(1, 100):
+        if not(i % 3) and not (i % 5):
+            print("FizzBuzz", end=", ")
+            continue
+        if not (i % 5):
+            print("Buzz", end=", ")
+            continue
+        if not(i % 3):
+            print("Fizz", end=", ")
+            continue
+        print(i, end=", ")
+    else:
+        i += 1
+        if not(i % 3) and not (i % 5):
+            print("FizzBuzz", end='')
+        if not (i % 5):
+            print("Buzz", end='')
+        if not(i % 3):
+            print("Fizz", end='')
