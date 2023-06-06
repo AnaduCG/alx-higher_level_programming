b448029eef08db744c0f1228792d0de12712f827
AnaduCG <godzionwin@gmail.com>
2023-06-06 18:35:39 +0100

    Getting rid of ',''s

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -2,15 +2,15 @@
 def fizzbuzz():
     for i in range(1, 100):
         if not(i % 3) and not (i % 5):
-            print("FizzBuzz", end=", ")
+            print("FizzBuzz", end=" ")
             continue
         if not (i % 5):
-            print("Buzz", end=", ")
+            print("Buzz", end=" ")
             continue
         if not(i % 3):
-            print("Fizz", end=", ")
+            print("Fizz", end=" ")
             continue
-        print(i, end=", ")
+        print(i, end=" ")
     else:
         i += 1
         if not(i % 3) and not (i % 5):
