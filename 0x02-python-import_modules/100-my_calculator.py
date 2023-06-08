c02f590b5bd5c9683ddf976a8037ac318bf47287
AnaduCG <godzionwin@gmail.com>
2023-06-08 13:16:03 +0100

    adding pycodestyle

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,21 +1,26 @@
 #!/usr/bin/python3
-import sys
-arg = sys.argv
+def b_calculator():
+    import sys
+    arg = sys.argv
 
-if len(arg) != 4:
-    print("Usage: {} <a> <operator> <b>".format(arg[0]))
-    exit(1)
-a = arg[1]
-b = arg[3]
-sn = arg[2]
-if sn == '+':
-    print("{} + {} = {}".format(a, b, a + b))
-elif sn == '-':
-    print("{} - {} = {}".format(a, b, a - b))
-elif sn == '*':
-    print("{} * {} = {}".format(a, b, a * b))
-elif sn == '/':
-    print("{} / {} = {}".format(a, b, a / b))
-else:
-    print("Unknown operator. Available operators: +, -, * and /")
-    exit(1)
+    if len(arg) != 4:
+        print("Usage: {} <a> <operator> <b>".format(arg[0]))
+        exit(1)
+    a = arg[1]
+    b = arg[3]
+    sn = arg[2]
+    if sn == '+':
+        print("{} + {} = {}".format(a, b, a + b))
+    elif sn == '-':
+        print("{} - {} = {}".format(a, b, a - b))
+    elif sn == '*':
+        print("{} * {} = {}".format(a, b, a * b))
+    elif sn == '/':
+        print("{} / {} = {}".format(a, b, a / b))
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
+
+
+if __name__ == "__main__":
+    b_calculator()
