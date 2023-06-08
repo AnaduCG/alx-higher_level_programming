8493734ab51fb790ad75999fbc98eb9244eebe03
AnaduCG <godzionwin@gmail.com>
2023-06-08 13:26:44 +0100

    adding requirement 'code should not be executed when imported'

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3
-from calculator_1 import *
-a = 10
-b = 5
-print("10 + 5 = {}".format(add(a, b)))
-print("10 - 5 = {}".format(sub(a, b)))
-print("10 * 5 = {}".format(mul(a, b)))
-print("10 / 5 = {}".format(div(a, b)))
+def f_calc():
+    from calculator_1 import *
+    a = 10
+    b = 5
+    print("10 + 5 = {}".format(add(a, b)))
+    print("10 - 5 = {}".format(sub(a, b)))
+    print("10 * 5 = {}".format(mul(a, b)))
+    print("10 / 5 = {}".format(div(a, b)))
+
+
+if __name__ == "__main__":
+    f_calc()
