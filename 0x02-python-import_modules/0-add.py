5cdca9dd1145bd5e7ba3f498f29e3f242731747f
AnaduCG <godzionwin@gmail.com>
2023-06-08 12:53:24 +0100

    Format shebang

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -3,7 +3,7 @@ def print_sum():
     from add_0 import add
     a = 1
     b = 2
-    print("i{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
 
 
 if __name__ == "__main__":
