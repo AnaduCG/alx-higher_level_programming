c0cb0f2cb6c5abcc05f2ddc0155b981d6576e5fa
AnaduCG <godzionwin@gmail.com>
2023-06-08 12:05:23 +0100

    First push

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+import sys
+arg = sys.argv
+
+if len(arg) != 4:
+    print("Usage: {} <a> <operator> <b>".format(arg[0]))
+    exit(1)
+a = arg[1]
+b = arg[3]
+sn = arg[2]
+if sn == '+':
+    print("{} + {} = {}".format(a, b, a + b))
+elif sn == '-':
+    print("{} - {} = {}".format(a, b, a - b))
+elif sn == '*':
+    print("{} * {} = {}".format(a, b, a * b))
+elif sn == '/':
+    print("{} / {} = {}".format(a, b, a / b))
+else:
+    print("Unknown operator. Available operators: +, -, * and /")
+    exit(1)
---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+import sys
+j = 0
+for i in sys.argv[1:]:
+    j += int(i)
+print(j)
