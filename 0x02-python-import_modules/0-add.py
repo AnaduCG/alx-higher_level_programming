fe21b5cfbc26784ab8860b2ba223169ef44e7baa
AnaduCG <godzionwin@gmail.com>
2023-06-08 12:51:28 +0100

    added required feature 'code should not be executed when imported - by using __import__'

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -3,7 +3,7 @@ def print_sum():
     from add_0 import add
     a = 1
     b = 2
-    print("1 + 2 = {:d}".format(add(a, b)))
+    print("i{:d} + {:d} = {:d}".format(a, b, add(a, b)))
 
 
 if __name__ == "__main__":
