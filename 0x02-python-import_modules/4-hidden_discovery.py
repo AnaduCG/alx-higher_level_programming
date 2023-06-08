e5af66d03bfa3c5ae492fdfdea500a2c39da2980
AnaduCG <godzionwin@gmail.com>
2023-06-08 14:00:15 +0100

    added pycodestyle indentation

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,8 +2,10 @@
 def hd():
     from hidden_4 as hidden
     hidden_names = dir(hidden)
+    hidden_names = sorted(hidden_names)
     for i in hidden_names:
-        print("{}".format(hidden_names))
+        if i[0] != '_':
+            print("{}".format(hidden_names))
 
 
 if __name__ == "__main__":
