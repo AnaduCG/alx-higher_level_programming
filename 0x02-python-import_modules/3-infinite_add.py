d5cf2e2102b2bdc121ca1a159ac82d7595ffd495
AnaduCG <godzionwin@gmail.com>
2023-06-08 13:11:55 +0100

    adding pycodestyle

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
-import sys
-j = 0
-for i in sys.argv[1:]:
-    j += int(i)
-print(j)
+def infite():
+    import sys
+    j = 0
+    for i in sys.argv[1:]:
+        j += int(i)
+    print(j)
+
+
+if __name__ == "__main__":
+    infite()
