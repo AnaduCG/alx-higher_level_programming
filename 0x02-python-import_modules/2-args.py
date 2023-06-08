9fc62cf9130541166206e8e731d699faed7dc574
AnaduCG <godzionwin@gmail.com>
2023-06-08 13:40:16 +0100

    Removing the ':d' format specifier

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,11 +3,11 @@ def sys_arg():
     import sys
     sys.argv.pop(0)
     if len(sys.argv) == 1:
-        print("{:d} argument:".format(len(sys.argv)))
+        print("{} argument:".format(len(sys.argv)))
     else:
-        print("{:d} arguments:".format(len(sys.argv)))
+        print("{} arguments:".format(len(sys.argv)))
     for i in range(len(sys.argv)):
-        print("{:d}: {:d}".format(i + 1, sys.argv[i]))
+        print("{}: {}".format(i + 1, sys.argv[i]))
 
 
 if __name__ == "__main__":
