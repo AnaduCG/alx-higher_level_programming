2c003faeb244a192045a46c0dc7f16a397ec1d9e
AnaduCG <godzionwin@gmail.com>
2023-06-10 21:14:55 +0100

    Optitmizing and refactoring code

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -6,11 +6,10 @@
 
 
 def print_matrix_integer(matrix=[[]]):
-    """ if len(matrix) <= 0:
-        print("$")
-    else: """
+    if not matrix:
+        print('$')
+        return
     for i in matrix:
         for j in i:
             print("{:2d}".format(j), end='')
-        else:
-            print("$")
+        print('$')
