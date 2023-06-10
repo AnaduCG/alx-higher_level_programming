2df138ef9af9dcb61e913d5831e4f4a3adb8f9b4
AnaduCG <godzionwin@gmail.com>
2023-06-10 21:03:55 +0100

    Added pycodestyle indentation

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 """print_reversed_list_integer() - a function that
                                 prints the content of an array in reverse order
     my_list=[]: the list to be handled by the function
@@ -9,4 +10,4 @@ def print_reversed_list_integer(my_list=[]):
     if not my_list:
         return
     for i in reversed(my_list):
-            print("{:d}".format(i))
+        print("{:d}".format(i))
