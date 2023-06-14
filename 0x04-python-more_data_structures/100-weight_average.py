af23c9fdea62753da7debed33d3f87ac75d00a5d
AnaduCG <godzionwin@gmail.com>
2023-06-14 23:12:22 +0100

    Updated lambda styling

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
-        s = 0
-        for i in my_list:
-                s += sum(i)
-        return (s / len(my_list))
+    s = 0
+    for i in my_list:
+        s += sum(i)
+    return (s / len(my_list))
