e982971bf43b25503c6baf926ea100e25331cc29
AnaduCG <godzionwin@gmail.com>
2023-06-14 21:15:23 +0100

    Correct pycodestyle indentation

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -4,6 +4,6 @@ def complex_delete(a_dictionary, value):
     for key, val in a_dictionary.items():
         if value == val:
             keys_to_delete.append(key)
-        for key in keys_to_delete:
-            del a_dictionary[key]
-        return a_dictionary
+    for key in keys_to_delete:
+        del a_dictionary[key]
+    return a_dictionary
