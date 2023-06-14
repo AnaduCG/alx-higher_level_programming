cd6c9428a82a1c53dba9c3b13ce8c6922da57b54
AnaduCG <godzionwin@gmail.com>
2023-06-15 00:37:10 +0100

    Refactoring code

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
-    s = 0
-    for i in my_list:
-        s += sum(i)
-    return (s / len(my_list))
+    if not list:
+        return 0
+    weight = sum(score * weight for score, weight in my_list)
+    total = sum(weight for _, weight in my_list)
+    return weight / total
