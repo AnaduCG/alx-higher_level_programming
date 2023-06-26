45076a2132e452a5cdade6d143a228a973ac0153
AnaduCG <godzionwin@gmail.com>
2023-06-26 21:28:16 +0100

    Updated: stderr to use Exception for error handling

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -10,10 +10,9 @@ def safe_print_integer_err(value):
         bool: True if the value is an integer and printed
         successfully, False otherwise.
     """
-    err = "Exception: Unknown format code 'd' for object of type 'str'\n"
     try:
         print("{:d}".format(value))
         return True
-    except (ValueError, TypeError):
-        sys.stderr.write("{}".format(err))
+    except Exception as err:
+        sys.stderr.write("{}\n".format(err))
         return False
