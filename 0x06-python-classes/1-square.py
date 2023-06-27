3466f27259a5079c94f6603abae6690e4c1de253
AnaduCG <godzionwin@gmail.com>
2023-06-27 18:44:03 +0100

    First commit

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+
+class Square:
+    """a class that defines a square
+    """
+    def __init__(self, size=0):
+        """Instantiation with optional size
+
+        Args:
+            size (int): variable for the size of the square class
+        """
+        self.__size = size
---
+++ 0x06-python-classes/README.md
@@ -0,0 +1,2 @@
+# 0x06. Python - Classes and Objects
+My ALX software engineering on Object Oriented Programming using python programming language
