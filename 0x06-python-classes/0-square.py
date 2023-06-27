7c5322a438bd047956b0b0abd389bdbbdafa8ff1
AnaduCG <godzionwin@gmail.com>
2023-06-27 21:23:43 +0100

    Update: code cleanup

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,10 +1,4 @@
 #!/usr/bin/python3
-
-
 class Square:
-    """This is just an empty class for the purpose of
-        demonstration on hoe to create a class in python
-        the function defines a square
-    """
-
+    """ an empty class Square that defines a square"""
     pass
