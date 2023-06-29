8b27a64c46a0dcfa2c739ee83b0ae6cd00ac08b2
AnaduCG <godzionwin@gmail.com>
2023-06-29 01:06:14 +0100

    Delete: removed unwanted files and cleanup

--- 0x06-python-classes/learn_OOP/0-square.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-# -*- coding: utf-8 -*-
-"""A simple representation of a class in python python programming
-"""
-
-class Square:
-    """ an empty class Square that defines a square:
-    """
-    pass
