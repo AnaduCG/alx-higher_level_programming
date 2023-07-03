8c5bd207214d8488cc73596b8b366ecd4985e300
AnaduCG <godzionwin@gmail.com>
2023-07-03 22:48:00 +0100

    New: First Commit

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""A basic representation of a class in python
+"""
+
+
+class Rectangle:
+    """an empty class Rectangle
+        that defines a rectangle
+    """
+    pass
---
+++ 0x08-python-more_classes/README.md
@@ -0,0 +1,2 @@
+# 0x08-python-more_classes
+My ALX software engineering on 0x08-python-more_classes
